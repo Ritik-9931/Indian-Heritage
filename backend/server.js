0e9c4db47914c9da12b60ee2ad21f12e5bb92d50
Ritik Raushan <raushanritikkrk7372@gmail.com>
2026-06-07 12:38:13 +0530

    correct

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ import stateRoutes from "./routes/stateRoutes.js";
 
 import ritualRoutes from "./routes/ritualRoutes.js";
 
-import userRoutes from "./routes/userRouter.js"
+import userRoutes from "./routes/userRouter.js";
 
 dotenv.config();
 
@@ -26,7 +26,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://indian-heritage-fj4y.onrender.com/api",
 });
 
 export default api;
